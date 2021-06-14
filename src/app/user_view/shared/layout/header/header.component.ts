5eb2af53f1d6a369a91f0a144ad49bb04121a257
sonhai32 <hailam32.dev@gmail.com>
2021-06-14 11:18:35 +0000

    create login-form for desktop & mobile

--- src/app/user_view/shared/layout/header/header.component.ts
+++ src/app/user_view/shared/layout/header/header.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NzDrawerPlacement } from 'ng-zorro-antd/drawer';
+import { DeviceDetectorService } from 'ngx-device-detector';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  isMobile: boolean = false;
+  expandLoginVisible = false;
+  expandRegisterExample = false;
+  drawerPlacement: NzDrawerPlacement = 'bottom'
+  constructor(private dvDectectorService: DeviceDetectorService) {}
 
   ngOnInit(): void {
+    this.isMobile = this.dvDectectorService.isMobile();
   }
 
+  openLogin(): void {
+    this.expandLoginVisible = true;
+  }
+  openRegister(): void {
+    this.expandRegisterExample = true;
+  }
+
+  closeLogin(): void {
+    this.expandLoginVisible = false;
+  }
+  closeRegister(): void {
+    this.expandRegisterExample = false;
+  }
 }
