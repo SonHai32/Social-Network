2f1d509b211e71f27cdb9ebe553d117a0fb52cae
sonhai32 <hailam32.dev@gmail.com>
2021-07-04 15:38:50 +0700

    save update

--- src/app/user_view/services/auth/auth-with-firebase.service.ts
+++ src/app/user_view/services/auth/auth-with-firebase.service.ts
@@ -1,13 +1,13 @@
 import { User } from './../../models/user.model';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Injectable } from '@angular/core';
-import firebase from 'firebase';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { UserCredentials } from '../../models/user-credentials.model';
 
 @Injectable({
   providedIn: 'root',
 })
+
 export class AuthWithFirebaseService {
   constructor(private auth: AngularFireAuth, private afs: AngularFirestore) {}
   async loginWithEmailAndPassword(user: UserCredentials): Promise<any | null> {
@@ -21,25 +21,28 @@ export class AuthWithFirebaseService {
       return null;
     }
   }
-  async createUserWithEmailAndPassword(userCredentials: UserCredentials): Promise<any | null> {
+
+  async createUserWithEmailAndPassword(
+    userCredentials: UserCredentials
+  ): Promise<any | null> {
     const res = await this.auth.createUserWithEmailAndPassword(
       userCredentials.email,
       userCredentials.password
     );
     if (res.user) {
-      const resUser: User = {id: res.user.uid, email: userCredentials.email, fullName: userCredentials.fullname}
-      const saveUser = await  (await (await (await this.afs.collection<User>('users').add(resUser)).get()).data())
-
-      if(saveUser){
-        console.log(saveUser)
-        return saveUser
+      const resUser: User = {
+        id: res.user.uid,
+        email: userCredentials.email,
+        fullName: userCredentials.fullname,
+      };
+      const saveUser = await await (
+        await (await this.afs.collection<User>('users').add(resUser)).get()
+      ).data();
+      if (saveUser) {
+        return saveUser;
       }
-
     } else {
       return null;
     }
   }
-
-
-
 }
