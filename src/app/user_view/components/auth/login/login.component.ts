d9eb4149c1e98f39405f545e1d81de191bb54c74
sonhai32 <hailam32.dev@gmail.com>
2021-07-10 16:50:37 +0700

    completed login-form-validation

--- src/app/user_view/components/auth/login/login.component.ts
+++ src/app/user_view/components/auth/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit,} from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'user-auth-login',
@@ -6,8 +7,36 @@ import { Component, OnInit,} from '@angular/core';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  validateForm!: FormGroup;
+  constructor(private fb: FormBuilder) {}
+
+  getEmailErrorTooltip(): string {
+    if (this.validateForm.controls.email.hasError('required')) {
+      return 'Vui lòng nhập email !';
+    } else if (this.validateForm.controls.email.invalid) {
+      return 'Email không hợp lệ !';
+    }
+    return '';
+  }
+
+  getPasswordErrorTooltip(): string {
+    if (this.validateForm.controls.password.invalid) {
+      return 'Vui lòng nhập mật khẩu';
+    }
+    return '';
+  }
 
   ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required]],
+    });
   }
-} 
+
+  submitForm(): void {
+    for (const i in this.validateForm.controls) {
+      this.validateForm.controls[i].markAsDirty();
+      this.validateForm.controls[i].updateValueAndValidity();
+    }
+  }
+}
