215bd536352f59a49239978e70d048aed9259fbd
sonhai32 <hailam32.dev@gmail.com>
2021-07-21 12:46:56 +0700

    add Loading State in AppState

--- src/app/user_view/store/app.state.ts
+++ src/app/user_view/store/app.state.ts
@@ -1,7 +1,9 @@
+import { LoadingState } from './app-loading/loading.state';
 import { ErrorState } from './error/error.state';
 import { AuthState } from './auth/auth.state';
 
 export interface AppState{
   auth_feature: AuthState,
   error_feature: ErrorState
+  loading_feature: LoadingState
 }
