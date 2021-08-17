e9c33f252e71bf6629f41b47a5e5ec1a3ab771e2
sonhai32 <hailam32.dev@gmail.com>
2021-08-17 17:21:19 +0700

    remove unused funtion

--- src/app/user_view/components/home/home.component.ts
+++ src/app/user_view/components/home/home.component.ts
@@ -1,28 +1,9 @@
-import { ActivatedRoute } from '@angular/router';
-import { PostsService } from './../../services/posts.service';
-import { getAuthSelector, getUserSelector } from './../../store/auth/auth.selectors';
+import { getUserSelector } from './../../store/auth/auth.selectors';
 import { User } from './../../models/user.model';
 import { AppState } from './../../store/app.state';
-import {
-  GetAllPost,
-  PostsActions,
-} from './../../store/posts/posts.actions';
-import {
-  getPostLoading,
-  PostSelectors,
-} from './../../store/posts/posts.selectors';
-import { select, Store } from '@ngrx/store';
-import { Observable, Subscription } from 'rxjs';
-import { Post } from './../../models/post.model';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { vmFromLatest } from '../../utils/operators';
-import { getPostsSelector } from '../../store/posts/posts.selectors';
-interface PostListVm {
-  posts: Post[];
-  isLoading: boolean;
-  currentUser?: User | null;
-  limit: number;
-}
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -30,10 +11,8 @@ interface PostListVm {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent implements OnInit {
-  constructor(private store: Store<AppState>,private postService: PostsService, private route: ActivatedRoute) {}
+  constructor(private store: Store<AppState>) {}
 
-  totalPost!: number;
-  postLimit!: number;
   currentUser$!: User| null;
 
   ngOnDestroy(): void {
@@ -43,39 +22,8 @@ export class HomeComponent implements OnInit {
   }
   subscription: Subscription = new Subscription();
 
-  vm$!: Observable<PostListVm>;
   ngOnInit(): void {
-    this.store.select(getUserSelector).subscribe(user => this.currentUser$ = user)
-    this.route.root.url.subscribe(val => console.log(val));
-    this.subscription.add(this.store.dispatch(GetAllPost({ limit: 5 })));
-    this.subscription.add(
-      this.postService.getTotalPost().subscribe((total: number) =>{
-        this.totalPost = total;
-      })
-
-    );
-    this.subscription.add(
-      this.store
-        .select(PostSelectors.getPostLimit)
-        .subscribe((limit: number) => {
-          this.postLimit = limit;
-        })
-    );
-    this.vm$ = vmFromLatest<PostListVm>({
-      posts: this.store.pipe(select(getPostsSelector)),
-      isLoading: this.store.pipe(select(getPostLoading)),
-      currentUser: this.store.pipe(select(getUserSelector)),
-      limit: this.store.pipe(select(PostSelectors.getPostLimit)),
-    });
+    this.subscription.add(this.store.select(getUserSelector).subscribe(user => this.currentUser$ = user))
   }
 
-  scrollHandle(position: any) {
-    if (position === 'bottom') {
-      if (this.postLimit < this.totalPost) {
-        this.store.dispatch(
-          PostsActions.GetAllPost({ limit: this.postLimit + 5 })
-        );
-      }
-    }
-  }
 }
