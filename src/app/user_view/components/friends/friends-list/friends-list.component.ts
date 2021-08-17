a223ebd8604a3396267dae154524c9631726a5ba
sonhai32 <hailam32.dev@gmail.com>
2021-08-17 13:22:54 +0700

    update friendlist UI

--- src/app/user_view/components/friends/friends-list/friends-list.component.ts
+++ src/app/user_view/components/friends/friends-list/friends-list.component.ts
@@ -1,21 +1,23 @@
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
 import { UserService } from 'src/app/user_view/services/user.service';
-import { Observable } from 'rxjs';
-import { Component, OnInit, Input } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { User } from 'src/app/user_view/models/user.model';
 
 @Component({
   selector: 'app-friends-list',
   templateUrl: './friends-list.component.html',
-  styleUrls: ['./friends-list.component.scss']
+  styleUrls: ['./friends-list.component.scss'],
 })
 export class FriendsListComponent implements OnInit {
-  @Input('currentUser') currentUser$!: User
-  constructor(private userService: UserService) { }
-  friendList$!: Observable<User[]>
-
+  @Input('currentUser') currentUser$!: User;
+  constructor(private userService: UserService) {}
+  friendList$!: Observable<User[]>;
+  friendLimit: number = 8;
+  searchFilter: string = '';
   ngOnInit(): void {
-    if(this.currentUser$){
-      this.friendList$ = this.userService.getAllFriend(this.currentUser$.id)
+    if (this.currentUser$) {
+      this.friendList$ = this.userService.getAllFriend(this.currentUser$.id);
     }
   }
 
