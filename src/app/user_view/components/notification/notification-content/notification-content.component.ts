5fc5a594c113dd72e4c166eed946d0c6819ff7ba
sonhai32 <hailam32.dev@gmail.com>
2021-08-18 14:03:51 +0700

    create notification-component

---
+++ src/app/user_view/components/notification/notification-content/notification-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notification-content',
+  templateUrl: './notification-content.component.html',
+  styleUrls: ['./notification-content.component.scss']
+})
+export class NotificationContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
