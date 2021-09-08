aa4c7aa796e1db6dfb3fb601b4dc14db083e4908
sonhai32 <hailam32.dev@gmail.com>
2021-09-08 18:13:08 +0700

    update message modal

--- src/app/social-network/models/message.model.ts
+++ src/app/social-network/models/message.model.ts
@@ -1,9 +1,8 @@
-import { User } from 'src/app/social-network/models/user.model';
 import { NzImage } from 'ng-zorro-antd/image';
-export interface Message{
+export interface PrivateMessage {
   id?: string,
-  messageText?: string,
-  messageImages?: NzImage[],
-  created_at: firebase.default.firestore.Timestamp ,
-  send_by: User
+  sendByID: string;
+  textMessage?: string;
+  imageMessage?: NzImage[];
+  created_at: firebase.default.firestore.Timestamp;
 }
