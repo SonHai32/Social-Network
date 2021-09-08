2c6329c73cdc59de4ead90c4d98ff6c75300dd57
sonhai32 <hailam32.dev@gmail.com>
2021-09-08 18:15:54 +0700

    update message

--- src/app/social-network/components/message/message.component.ts
+++ src/app/social-network/components/message/message.component.ts
@@ -3,20 +3,15 @@ import { tap } from 'rxjs/operators';
 import { vmFromLatest } from 'src/app/social-network/utils/operators';
 import { AppMessageAction } from '../../store/app-message/app-message.actions';
 import { ActivatedRoute } from '@angular/router';
-import { Message } from '../../models/message.model';
+import { PrivateMessage } from '../../models/message.model';
 import { getUserSelector } from 'src/app/social-network/store/auth/auth.selectors';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { MessageService } from '../../services/message.service';
 import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { User } from '../../models/user.model';
 import { Store, select } from '@ngrx/store';
 import firebase from 'firebase/app';
-
-interface MessageVM {
-  currentUser: User;
-  messages: Message[];
-}
 
 @Component({
   selector: 'app-message',
@@ -34,28 +29,35 @@ export class MessageComponent implements OnInit {
   ) {}
   chatBoxValue: string | null = null;
   currentUser$!: User;
-  friend$!: Observable<User>;
-  friendID!: string;
-  messageVM$!: Observable<MessageVM>;
+  friend!: User;
+  messages$!: Observable<PrivateMessage[]>;
+  subscription: Subscription = new Subscription();
+
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    this.subscription.unsubscribe();
+  }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params) => {
       const ID: string | null = params.get('id');
       if (ID) {
-        this.friend$ = this.userService.getUserInfo(ID);
-        this.friendID = ID;
-
-        this.store.select(getUserSelector).subscribe((user) => {
-          if (user) {
-            this.currentUser$ = user;
-            this.messageVM$ = vmFromLatest<MessageVM>({
-              currentUser: new Observable((sub) => sub.next(user)),
-              messages: this.messageService
-                .getMessage(user.id, this.friendID)
-                .pipe(tap(() => this.scrollToBottom())),
-            });
-          }
-        });
+        this.subscription.add(
+          this.userService.getUserInfo(ID).subscribe((user: User) => {
+            this.friend = user;
+          })
+        );
+        this.subscription.add(
+          this.store.select(getUserSelector).subscribe((user) => {
+            if (user) {
+              this.messages$ = this.messageService
+                .getMessage(user.id, ID)
+                .pipe(tap(() => this.scrollToBottom()));
+              this.currentUser$ = user;
+            }
+          })
+        );
       }
     });
   }
@@ -64,14 +66,14 @@ export class MessageComponent implements OnInit {
     return (Date.now() - messageTime.getTime()) / 3600000 > 1;
   }
   sendMessage(chatBoxEl: HTMLTextAreaElement) {
-    if (this.currentUser$ && this.friendID) {
-      const message: Message = {
-        send_by: this.currentUser$,
+    if (this.currentUser$ && this.friend) {
+      const message: PrivateMessage = {
+        sendByID: this.currentUser$.id,
         created_at: firebase.firestore.Timestamp.now(),
-        messageText: this.chatBoxValue ? this.chatBoxValue : '',
+        textMessage: this.chatBoxValue ? this.chatBoxValue : '',
       };
       this.messageService
-        .sendMessage(this.currentUser$, this.friendID, message)
+        .sendMessage(this.currentUser$, this.friend.id, message)
         .then(() => {
           chatBoxEl.blur();
           this.chatBoxValue = '';
--- src/app/social-network/services/message.service.ts
+++ src/app/social-network/services/message.service.ts
@@ -1,6 +1,6 @@
+import { PrivateMessage } from './../models/message.model';
 import { NotificationService } from './notification.service';
 import { map } from 'rxjs/operators';
-import { Message } from '../models/message.model';
 import { AngularFirestore, DocumentReference } from '@angular/fire/firestore';
 import { Injectable } from '@angular/core';
 import { User } from '../models/user.model';
@@ -16,10 +16,19 @@ export class MessageService {
     private notificationService: NotificationService
   ) {}
 
+  getMessageList(currentUserID: string): Observable<string[]> {
+    return this.afs
+      .doc<User>(`users/${currentUserID}`)
+      .collection('messages')
+      .stateChanges(['added', 'removed'])
+      .pipe(map((res) => [...res.length.toString()]));
+
+  }
+
   sendMessage(
     fromUser: User,
     toUserID: string,
-    message: Message
+    message: PrivateMessage
   ): Promise<[DocumentReference, DocumentReference, DocumentReference]> {
     return Promise.all([
       this.saveMessage(fromUser.id, toUserID, message),
@@ -34,20 +43,20 @@ export class MessageService {
     ]);
   }
 
-  saveMessage(fromuserID: string, toUserID: string, message: Message) {
+  saveMessage(fromuserID: string, toUserID: string, message: PrivateMessage) {
     return this.afs
       .doc<User>(`users/${fromuserID}`)
       .collection('messages')
       .doc(toUserID)
-      .collection<Message>('message_content')
+      .collection<PrivateMessage>('message_content')
       .add(message);
   }
-  getMessage(fromUserID: string, toUserID: string): Observable<Message[]> {
+  getMessage(fromUserID: string, toUserID: string): Observable<PrivateMessage[]> {
     return this.afs
       .doc<User>(`users/${fromUserID}`)
       .collection('messages')
       .doc(toUserID)
-      .collection<Message>('message_content', (ref) =>
+      .collection<PrivateMessage>('message_content', (ref) =>
         ref.orderBy('created_at', 'asc')
       )
       .snapshotChanges(['added', 'removed'])
@@ -57,7 +66,7 @@ export class MessageService {
             return {
               ...message.payload.doc.data(),
               id: message.payload.doc.id,
-            } as Message;
+            } as PrivateMessage;
           })
         )
       );
