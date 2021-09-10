564d5e44fd68d9bc9c7ca83629547de7bf2b887f
sonhai32 <hailam32.dev@gmail.com>
2021-09-10 22:06:18 +0700

    update footer-component

--- src/app/social-network/shared/layout/footer/footer.component.ts
+++ src/app/social-network/shared/layout/footer/footer.component.ts
@@ -1,15 +1,27 @@
+import { Router } from '@angular/router';
+import { AuthActions } from './../../../store/auth/auth.action';
+import { getUserSelector } from 'src/app/social-network/store/auth/auth.selectors';
+import { User } from 'src/app/social-network/models/user.model';
+import { Store } from '@ngrx/store';
 import { NavService } from './../../../services/nav.service';
 import { Component, OnInit } from '@angular/core';
 import { NavOptions } from 'src/app/social-network/app-models/nav-options.model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
-
-  constructor(private navService: NavService) { }
+  isAuthenticated: boolean = false;
+  currentUser$!: Observable<User | null>;
+  extendMenuVisible: boolean = false;
+  constructor(
+    private navService: NavService,
+    private store: Store,
+    private router: Router
+  ) {}
 
   toggleNavOptions(
     option: 'messages' | 'notifications' | 'friends' | 'search'
@@ -32,6 +44,22 @@ export class FooterComponent implements OnInit {
     }
   }
   ngOnInit(): void {
+    this.currentUser$ = this.store.select(getUserSelector);
+  }
+  toggleExtendMenu() {
+    this.extendMenuVisible = !this.extendMenuVisible;
+  }
+  logOut() {
+    this.store.dispatch(AuthActions.Logout());
   }
 
+  toggleNavigation(path: 'user' | 'friends', userID?: string) {
+    if (path === 'user' && userID) {
+      this.router.navigate(['/user', userID]);
+      this.extendMenuVisible = false;
+    } else {
+      this.router.navigate(['/friends']);
+      this.extendMenuVisible = false;
+    }
+  }
 }
