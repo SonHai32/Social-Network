92dd3c48dec462ba1fa84404abe7614613afa4c3
sonhai32 <hailam32.dev@gmail.com>
2021-09-10 18:19:49 +0700

    Header: clean code

--- src/app/social-network/shared/layout/header/header.component.ts
+++ src/app/social-network/shared/layout/header/header.component.ts
@@ -1,120 +1,34 @@
-import { Router } from '@angular/router';
-import { NzNotificationService } from 'ng-zorro-antd/notification';
-import { Notification } from './../../../models/notification.model';
 import { tap } from 'rxjs/operators';
-import { vmFromLatest } from 'src/app/social-network/utils/operators';
-import { Post } from '../../../models/post.model';
-import { UserService } from 'src/app/social-network/services/user.service';
-import { NotificationService } from '../../../services/notification.service';
-import { AppMessageAction } from '../../../store/app-message/app-message.actions';
 import { AuthActions } from '../../../store/auth/auth.action';
 import {
   getUserSelector,
-  getAuthSelector,
 } from '../../../store/auth/auth.selectors';
 import { Store } from '@ngrx/store';
-import { Observable, Subscription } from 'rxjs';
-import { DeviceDetectorService } from 'ngx-device-detector';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/social-network/models/user.model';
-import { PostsService } from 'src/app/social-network/services/posts.service';
-import firebase from 'firebase/app';
-interface SearchVM {
-  users: User[];
-  posts: Post[];
-}
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  searchVM$!: Observable<SearchVM>;
   authVisible: boolean = false;
   authFormSelected: string = 'LOGIN';
-  notificationVisible: boolean = false;
-  searchVisible: boolean = false;
-  isMobile: boolean = false;
-  currentUser!: User;
+  currentUser$!: Observable<User | null>;
   isAuthenticated!: boolean;
-  unseenNotification!: Observable<Notification[]>;
-  subscription: Subscription = new Subscription();
 
   constructor(
-    private dv: DeviceDetectorService,
     private store: Store,
-    private userNotificationService: NotificationService,
-    private userService: UserService,
-    private postService: PostsService,
-    private nzNotificationService: NzNotificationService,
-    private router: Router
   ) {}
 
   ngOnInit(): void {
-    this.isMobile = this.dv.isMobile();
-    this.subscription.add(
-      this.store.select(getAuthSelector).subscribe((authenticated: boolean) => {
-        this.isAuthenticated = authenticated;
-      })
-    );
-    this.subscription.add(
-      this.store.select(getUserSelector).subscribe((user: User | null) => {
-        if (user) {
-          this.currentUser = user;
-          this.unseenNotification = this.userNotificationService
-            .getUnseenNotification(user.id)
-            .pipe(
-              tap((notifications: Notification[]) => {
-                notifications.forEach((val) => {
-                  if (
-                    firebase.firestore.Timestamp.now().seconds -
-                      val.created_at.seconds <
-                    2
-                  ) {
-                    this.nzNotificationService.blank(
-                      val.byUser.display_name,
-                      val.title
-                    );
-                  }
-                });
-              })
-            );
-        }
-      })
-    );
-  }
-
-  toggleNav(path: 'messages' | 'notifications' | 'friends' | 'search') {
-    if (this.currentUser) {
-      switch (path) {
-        case 'messages':
-          this.router.navigate(['/messages']);
-          break;
-        case 'notifications':
-          this.toggleNotification();
-          break;
-        case 'friends':
-          this.router.navigate(['/friends']);
-          break;
-        case 'search':
-          this.toggleSearch();
-          break;
-        default:
-          return;
-      }
-    } else {
-      this.store.dispatch(
-        AppMessageAction.SetAppMessage({
-          message: 'Bạn chưa đăng nhập',
-          message_type: 'warning',
-        })
-      );
-    }
-  }
-  ngOnDestroy(): void {
-    //Called once, before the instance is destroyed.
-    //Add 'implements OnDestroy' to the class.
-    this.subscription.unsubscribe();
+   this.currentUser$ = this.store.select(getUserSelector).pipe(tap((user =>{
+     if(user){
+       this.isAuthenticated = true
+     }else
+     this.isAuthenticated = false
+   })))
   }
 
   toggleAuth(formSelect?: string): void {
@@ -123,31 +37,11 @@ export class HeaderComponent implements OnInit {
       this.changeAuthForm(formSelect);
     }
   }
-  toggleNotification() {
-    if (this.currentUser) {
-      this.notificationVisible = !this.notificationVisible;
-    } else {
-      this.store.dispatch(
-        AppMessageAction.SetAppMessage({
-          message: 'Bạn chưa đăng nhập',
-          message_type: 'warning',
-        })
-      );
-    }
-  }
-  toggleSearch() {
-    this.searchVisible = !this.searchVisible;
-  }
+
   changeAuthForm(formSelected: string): void {
     this.authFormSelected = formSelected;
   }
 
-  handleSearchChange(value: string) {
-    this.searchVM$ = vmFromLatest<SearchVM>({
-      users: this.userService.searchUser(value),
-      posts: this.postService.searchPost(value),
-    });
-  }
   logout(): void {
     this.store.dispatch(AuthActions.Logout());
   }
